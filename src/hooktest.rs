98c3d7dbf269e9aca0a3ef87d67c6854afcd0df9
Sundowns <tom@smallridge.com.au>
2019-03-19 00:20:23 +0800

    initial commit

---
+++ src/hooktest.rs
@@ -0,0 +1,95 @@
+extern crate amethyst;
+use amethyst::prelude::*;
+use amethyst::core::transform::Transform;
+use amethyst::ecs::prelude::{Component, DenseVecStorage};
+use amethyst::assets::{AssetStorage, Loader};
+use amethyst::renderer::{
+    Camera, PngFormat, Projection, SpriteRender, SpriteSheet, SpriteSheetFormat,
+    SpriteSheetHandle, Texture, TextureMetadata,
+};
+
+pub const ARENA_HEIGHT: f32 = 100.0;
+pub const ARENA_WIDTH: f32 = 100.0;
+pub const HOOK_RADIUS: f32 = 2.0;
+
+pub struct HookTest;
+
+impl SimpleState for HookTest {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
+
+        let sprite_sheet_handle = load_sprite_sheet(world);
+
+        world.register::<Hook>();
+
+        initialise_hook(world, sprite_sheet_handle);
+        initialise_camera(world);
+    }
+}
+
+pub struct Hook {
+    pub velocity: [f32; 2],
+    pub radius: f32,
+}
+
+impl Component for Hook {
+    type Storage = DenseVecStorage<Self>;
+}
+
+fn initialise_hook(world: &mut World, sprite_sheet: SpriteSheetHandle) {
+    let mut local_transform = Transform::default();
+    local_transform.set_xyz(ARENA_WIDTH / 2.0, ARENA_HEIGHT / 2.0, 0.0);
+
+    // Assign the sprite
+    let sprite_render = SpriteRender {
+        sprite_sheet: sprite_sheet.clone(),
+        sprite_number: 0, 
+    };
+
+    world.create_entity().with(sprite_render).with(Hook {
+        velocity: [0.0,0.0],
+        radius: HOOK_RADIUS
+    }).with(local_transform).build();
+}
+
+fn initialise_camera(world: &mut World) {
+    let mut transform = Transform::default();
+    transform.set_z(1.0);
+    world
+        .create_entity()
+        .with(Camera::from(Projection::orthographic(
+            0.0,
+            ARENA_WIDTH,
+            0.0,
+            ARENA_HEIGHT,
+        )))
+        .with(transform)
+        .build();
+}
+
+fn load_sprite_sheet(world: &mut World) -> SpriteSheetHandle {
+    // Load the sprite sheet necessary to render the graphics.
+    // The texture is the pixel data
+    // `texture_handle` is a cloneable reference to the texture
+    let texture_handle = {
+        let loader = world.read_resource::<Loader>();
+        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+        loader.load(
+            "texture/hook_spritesheet.png",
+            PngFormat,
+            TextureMetadata::srgb_scale(),
+            (),
+            &texture_storage,
+        )
+    };
+
+    let loader = world.read_resource::<Loader>();
+    let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
+    loader.load(
+        "texture/hook_spritesheet.ron", // Here we load the associated ron file
+        SpriteSheetFormat,
+        texture_handle, // We pass it the handle of the texture we want it to use
+        (),
+        &sprite_sheet_store,
+    )
+}
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+extern crate amethyst;
+
+use amethyst::{
+    prelude::*,
+    renderer::{DisplayConfig, DrawFlat2D, Pipeline, RenderBundle, Stage},
+    utils::application_root_dir,
+    core::transform::TransformBundle,
+};
+
+mod hooktest;
+use crate::hooktest::HookTest;
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    let path = format!(
+        "{}/resources/display_config.ron",
+        application_root_dir()
+    );
+    let config = DisplayConfig::load(&path);
+
+    let pipe = Pipeline::build().with_stage(
+        Stage::with_backbuffer()
+            .clear_target([1.0, 0.4862, 0.4, 1.0], 1.0)
+            .with_pass(DrawFlat2D::new()),
+    );
+
+    let game_data =
+        GameDataBuilder::default().with_bundle(RenderBundle::new(pipe, Some(config)).with_sprite_sheet_processor())?.with_bundle(TransformBundle::new())?;
+
+        let mut game = Application::new("./", HookTest, game_data)?;
+
+    game.run();
+
+    Ok(())
+}

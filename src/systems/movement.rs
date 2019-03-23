6b20f8f385b40fedfa422627f2d8b4ac7cf7cd6a
Sundowns <tom@smallridge.com.au>
2019-03-23 21:36:58 +0800

    spicing up movement

--- src/systems/movement.rs
+++ src/systems/movement.rs
@@ -5,6 +5,7 @@ use amethyst::ecs::{Join, Read, System, WriteStorage};
 use amethyst::input::InputHandler;
 
 use crate::components::Player;
+use crate::hookarena::{ARENA_HEIGHT, ARENA_WIDTH};
 
 pub struct MovementSystem;
 
@@ -21,10 +22,8 @@ impl<'s> System<'s> for MovementSystem {
             match input.axis_value("horizontal") {
                 Some(v) => {
                     let current_velocity_y = _player.velocity[0];
-                    _player.velocity[0] = (current_velocity_y
-                        + (v as f32 * _player.acceleration * time.delta_seconds()))
-                    // .min(-1.0 * _player.max_velocity[0])
-                    // .max(1.0 * _player.max_velocity[0]);
+                    _player.velocity[0] = current_velocity_y
+                        + (v as f32 * _player.acceleration * time.delta_seconds());
                 }
                 _ => {}
             }
@@ -32,19 +31,36 @@ impl<'s> System<'s> for MovementSystem {
             match input.axis_value("vertical") {
                 Some(v) => {
                     let current_velocity_x = _player.velocity[1];
-                    _player.velocity[1] = (current_velocity_x
-                        + (v as f32 * _player.acceleration * time.delta_seconds()))
-                    // .min(-1.0 * _player.max_velocity[1])
-                    // .max(1.0 * _player.max_velocity[1]);
+                    _player.velocity[1] = current_velocity_x
+                        + (v as f32 * _player.acceleration * time.delta_seconds());
                 }
                 _ => {}
             }
 
-            // TODO: why are my mins/max's fucking me and forcing it to the maximum?
+            // TODO: why are my mins/max's being weird
+            let player_x = _transform.translation().x;
+            _transform.set_x(
+                (player_x + (_player.velocity[0] * time.delta_seconds()))
+                    .min(ARENA_WIDTH)
+                    .max(0.0),
+            );
+            let player_y = _transform.translation().y;
+            _transform.set_y(
+                (player_y + (_player.velocity[1] * time.delta_seconds()))
+                    .min(ARENA_HEIGHT)
+                    .max(0.0),
+            );
 
-            _transform.translate_x(_player.velocity[0] * time.delta_seconds());
-            _transform.translate_y(_player.velocity[1] * time.delta_seconds());
-            println!("vel {}, {}", _player.velocity[0], _player.velocity[1]);
+            // _transform.translate_x(_player.velocity[0] * time.delta_seconds());
+            // _transform.translate_y(_player.velocity[1] * time.delta_seconds());
+            println!(
+                "pos {}, {} --- vel {}, {}",
+                _transform.translation().x,
+                _transform.translation().y,
+                _player.velocity[0],
+                _player.velocity[1]
+            );
+            // println!("pos {}, {}", _player.velocity[0], _player.velocity[1]);
         }
     }
 }

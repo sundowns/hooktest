3263306fb2ff07ffddb0f14f73835110a736c914
sundowns <tom@smallridge.com.au>
2019-04-08 22:55:44 +0800

    spawning a single hook based on mouse position. add a system to move the hooks and spawn with velocity next

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -2,9 +2,14 @@ use amethyst::ecs::prelude::{Component, DenseVecStorage};
 
 mod gravity;
 mod hook;
+mod hook_fired;
 mod player;
 
-pub use self::{gravity::Gravity, hook::Hook, player::Player};
+pub use self::{gravity::Gravity, hook::Hook, hook_fired::HookFired, player::Player};
+
+impl Component for HookFired {
+    type Storage = DenseVecStorage<Self>;
+}
 
 impl Component for Hook {
     type Storage = DenseVecStorage<Self>;
--- src/systems/aiming.rs
+++ src/systems/aiming.rs
@@ -1,14 +1,17 @@
 extern crate amethyst;
 
 use amethyst::core::Transform;
-use amethyst::ecs::{
-    Entities, Join, Read, ReadExpect, ReadStorage, Resources, System, WriteStorage,
-};
+use amethyst::ecs::{Entities, Entity, Join, Read, ReadExpect, ReadStorage, System, WriteStorage};
 use amethyst::input::InputHandler;
-use amethyst::renderer::{ScreenDimensions, SpriteRender, SpriteSheetHandle};
+use amethyst::renderer::{ScreenDimensions, SpriteRender};
 
-use crate::components::{Hook, Player};
+use crate::components::{Hook, HookFired, Player};
 use crate::hookarena::{GameAssets, ARENA_HEIGHT, ARENA_WIDTH, HOOK_RADIUS};
+
+pub struct NewHook {
+    owner: Entity,
+    transform: Transform,
+}
 
 pub struct AimingSystem;
 
@@ -22,43 +25,61 @@ impl<'s> System<'s> for AimingSystem {
         ReadExpect<'s, ScreenDimensions>,
         ReadExpect<'s, GameAssets>,
         WriteStorage<'s, SpriteRender>,
+        WriteStorage<'s, HookFired>,
     );
 
     fn run(
         &mut self,
-        (mut transforms, mut hooks, players, entities, input, screen, assets, mut sprites): Self::SystemData,
+        (
+            mut transforms,
+            mut hooks,
+            players,
+            entities,
+            input,
+            screen,
+            assets,
+            mut sprites,
+            mut hooks_fired,
+        ): Self::SystemData,
     ) {
         let screen_ratios = vec![ARENA_WIDTH / screen.width(), ARENA_HEIGHT / screen.height()];
-        let mut new_hooks: Vec<Transform> = Vec::new();
+        let mut new_hooks: Vec<NewHook> = Vec::new();
 
-        for (_player, _transform) in (&players, &mut transforms).join() {
+        for (_entity, _player, _, _) in (&entities, &players, &mut transforms, !&hooks_fired).join()
+        {
             match input.action_is_down("fire") {
                 Some(_v) => {
-                    if _v {
-                        if let Some(position) = input.mouse_position() {
-                            let mut local_transform = Transform::default();
-                            local_transform.set_xyz(
-                                ((position.0 as f32) * screen_ratios[0])
-                                    .min(ARENA_WIDTH)
-                                    .max(0.0),
-                                (ARENA_HEIGHT - ((position.1 as f32) * screen_ratios[1]))
-                                    .min(ARENA_HEIGHT)
-                                    .max(0.0),
-                                0.0,
-                            );
-                            new_hooks.push(local_transform);
-                        }
+                    if let Some(position) = input.mouse_position() {
+                        let mut local_transform = Transform::default();
+                        local_transform.set_xyz(
+                            ((position.0 as f32) * screen_ratios[0])
+                                .min(ARENA_WIDTH)
+                                .max(0.0),
+                            (ARENA_HEIGHT - ((position.1 as f32) * screen_ratios[1]))
+                                .min(ARENA_HEIGHT)
+                                .max(0.0),
+                            0.0,
+                        );
+                        new_hooks.push(NewHook {
+                            transform: local_transform,
+                            owner: _entity,
+                        });
                     }
                 }
                 _ => {}
             }
         }
 
-        while let Some(_transform) = new_hooks.pop() {
-            // TODO: Prevent more than 1 hook being fired at a time per player
+        while let Some(_new_hook) = new_hooks.pop() {
+            // TODO: spawn the hook at the players origin and give it velocity towards its target!
+            match hooks_fired.insert(_new_hook.owner, HookFired) {
+                Err(_v) => panic!("Failed to store new hook"),
+                _ => {}
+            };
+
             entities
                 .build_entity()
-                .with(_transform, &mut transforms)
+                .with(_new_hook.transform, &mut transforms)
                 .with(
                     Hook {
                         velocity: [0.0, 0.0],
